dd5d403468ba5709171fdc3f2e1d10ba84f491b2
Apollojk106 <149687810+Apollojk106@users.noreply.github.com>
2024-05-15 22:37:10 -0300

    Update AlterarSenhaClass.cs

--- KelleBolosBO/AlterarSenhaClass.cs
+++ KelleBolosBO/AlterarSenhaClass.cs
@@ -8,5 +8,44 @@ namespace KelleBolosBO
 {
     internal class AlterarSenhaClass
     {
+        
+        private string senhaAntiga { get; set; }
+
+        private string senhaNova { get; set; }
+
+        private string senhaConfirmar { get; set; }
+
+        private string Usuario { get; set; }
+
+        public AlterarSenhaClass(string usuario, string antigasenha, string senhanova, string senhaconfirmar)
+        {
+            senhaAntiga = senhaAntiga;
+            senhaNova = senhanova;
+            senhaConfirmar = senhaconfirmar;
+            Usuario = usuario;
+        }
+
+        public string AlterarSenha() 
+        {
+            BancoDeDadosClass Bd = new BancoDeDadosClass();
+            string senhaAnterior = Bd.LerInfo(Usuario,"Senha");
+
+            if (senhaNova != senhaConfirmar)
+            {
+                return "Senhas diferentes!";
+            }
+            else if (senhaNova == senhaAnterior)
+            {
+                return "Senha Igual a antiga!";
+            }
+            else 
+            {
+                if (Bd.AlterarSenha(Usuario)) {
+                    return "Senha Alterada!";
+                }
+
+                return "mal sucedido";
+            }   
+        }
     }
 }
